3060389e806a3560e5ab96aa2b700347e1df9b28
Manny Rivera <mcomp2010@yahoo.com>
2013-10-12 00:55:46 -0400

    Finish Question 4

--- depot/app/helpers/application_helper.rb
+++ depot/app/helpers/application_helper.rb
@@ -7,8 +7,32 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 module ApplicationHelper
-  
+
+
   def get_date()
-    @var = Time.now.strftime("%B %e, %Y at %I:%M %p");
+    @var = Time.now.strftime("%B %e, %Y at %I:%M %p")
+  end
+
+
+  def count()
+
+
+    if cookies[:init_date].nil?
+      cookies[:init_date] = Time.now.strftime("%B %e, %Y at %I:%M %p")
+    else
+         @days = DateTime.now.mjd -  DateTime.parse(cookies[:init_date]).mjd
+         cookies[:bet_date]=@days
+    end
+
+    if @days.nil?
+      @days=0;
+    end
+
+    if @days <= 120
+      @days_output = 'Initial Date:' + cookies[:init_date]
+    else
+      @days_output = 'Your last visited was too long ago'
+    end
+
   end
 end
